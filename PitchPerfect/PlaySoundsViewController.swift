b8789f06ebf859212500070e4ea8fb8d0ee29897
José Naves <josenaves@gmail.com>
2018-09-10 16:56:28 -0300

    Assign buttons to actions and outlets

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+
     var recordedAudioURL: URL!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("play sound button pressed")
+    }
+
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        print("stop audio button pressed")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +39,6 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
